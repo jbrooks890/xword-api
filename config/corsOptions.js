f62ca40e9c2001fd4595de2858fe4911abb42b1c
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-11-28 09:31:25 -0500

    heroku deployment testing

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -10,17 +10,22 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    console.log({ origin });
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
   optionsSuccessStatus: 200,
 };
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log({ origin });
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 
 // console.log("allowed origins:", allowedOrigins, "\ncors config:", corsOptions); //TODO
 
