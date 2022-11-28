5cef92a04fae862d3a776f2d7dc41d23d7254c60
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-11-27 21:04:19 -0500

    heroku deployment testing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 
 const app = express();
+console.log("corsOptions:", corsOptions);
 
 // app.use(cors());
 app.use(cors(corsOptions));
@@ -13,6 +14,7 @@ app.use(express.json()); // USE()?
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(cookieParser());
+app.get("/", (req, res) => res.json(corsOptions)); // TODO: remove
 app.use("/api", routes);
 
 module.exports = app;
