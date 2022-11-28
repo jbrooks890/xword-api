f68b283127e275af17513edccedc3630f74affbe
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-11-27 20:40:28 -0500

    heroku deployment testing

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
+  "https://xword-app.herokuapp.com/",
+  "http://xword-app.herokuapp.com/",
   "https://xword-app-beta.netlify.app/",
   "http://xword-app-beta.netlify.app/",
 ];
